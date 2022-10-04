d83558085f358652d7426c296ad6c10ac66a9958
panchalbhavya2210@gmail.com <panchalbhavya2210@gmail.com>
2022-10-04 22:52:10 +0530

    commit -perp%1

---
+++ main.js
@@ -0,0 +1,13 @@
+let navBarBtn = document.getElementById('navBarBtn');
+
+navBarBtn.onclick = function() {
+  let barOne = document.getElementById('barOne');
+  let barTwo = document.getElementById('barTwo');
+  let barThi = document.getElementById('barThi');
+   let navCall = document.getElementById('navCall');
+  
+  barOne.classList.toggle('barOneMove');
+  //barTwo.classList.toggle('');
+  barThi.classList.toggle('barThiMove');
+navCall.classList.toggle('widthNav');
+}
