dc308a0af130033dac5af7922520fd3d1cd7b433
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2022-12-20 20:32:06 +0530

    commit good

---
+++ main.js
@@ -0,0 +1,15 @@
+let day = new Date();
+
+let hours = day.getHours();
+
+let greetDayToUser = document.getElementById("greetDayToUser");
+
+if (hours == 00) {
+  greetDayToUser.innerHTML = "Good Morning";
+} else if (hours > 06 && hours < 12) {
+  greetDayToUser.innerHTML = "Good Morning";
+} else if (hours > 12 && hours < 17) {
+  greetDayToUser.innerHTML = "Good Afternoon";
+} else if (hours > 17 && hours < 23) {
+  greetDayToUser.innerHTML = "Good Evening";
+}
