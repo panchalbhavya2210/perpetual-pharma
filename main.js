0991fd3762793e9bc61ad923dd3ed0da97c242f7
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2022-12-21 22:21:21 +0530

    commit

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ let runWindSc = document.getElementById("runWindScroll");
 
 runWindSc.addEventListener("click", () => {
   window.scrollTo({
-    top: 555,
+    top: 535,
     behavior: "smooth",
   });
 });
