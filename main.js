2222f79f169292cd196e1c7ccb754883ea80e462
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2022-12-31 23:06:31 +0530

    bugged js

--- main.js
+++ main.js
@@ -22,3 +22,29 @@ runWindSc.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+// for (const clicker of medAccord) {
+//   clicker.addEventListener("click", () => {
+//     alert(1);
+//   });
+// }
+const medAccord = document.querySelectorAll(".medAccord");
+const accordMain = document.querySelectorAll(".accordionMainOne");
+
+function letAccordBtn() {
+  for (let i = 0; i < 18; i++) {
+    let classNames = new Array();
+    classNames[i] = accordMain[i].className;
+    // console.log(classNames[i])
+      accordMain[i].className = "accordionMainOne accordMain";
+
+      // if (accordMain[i].classList.contains("accordToggle")) {
+      //   accordMain[i].classList.remove("accordToggle");
+      // } else {
+      //   accordMain[i].classList.add("accordToggle");
+      // }
+    });
+  }
+}
+
+medAccord.onclick = letAccordBtn();
